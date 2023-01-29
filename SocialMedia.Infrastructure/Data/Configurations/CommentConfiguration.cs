3f4b127277000b97422c383660f67b40e53c3a02
BenjaCardozo <benjamin.cardozo0@gmail.com>
2023-01-28 23:18:22 -0300

    Primer Commit

---
+++ SocialMedia.Infrastructure/Data/Configurations/CommentConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMedia.Core.Entities;
+
+namespace SocialMedia.Infrastructure.Data.Configurations
+{
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable("Comentario");
+            builder.HasKey(e => e.CommentId);
+
+            builder.Property(e => e.CommentId)
+            .HasColumnName("IdComentario")
+            .ValueGeneratedNever();
+
+            builder.Property(e => e.PostId)
+            .HasColumnName("IdPublicacion")
+            .ValueGeneratedNever();
+
+            builder.Property(e => e.UserId)
+            .HasColumnName("IdUsuario")
+            .ValueGeneratedNever();
+
+            builder.Property(e => e.IsActive)
+            .HasColumnName("Activo");
+
+            builder.Property(e => e.Descripcion)
+                .IsRequired()
+                .HasColumnName("Descripcion")
+                .HasMaxLength(500)
+                .IsUnicode(false);
+
+            builder.Property(e => e.Date).HasColumnType("datetime")
+            .HasColumnName("Fecha");
+
+            builder.HasOne(d => d.Post)
+                .WithMany(p => p.Coments)
+                .HasForeignKey(d => d.PostId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Comentario_Publicacion");
+
+            builder.HasOne(d => d.User)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Comentario_Usuario");
+        }
+    }
+}
